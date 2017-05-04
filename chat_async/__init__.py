4c00cd705ce955ce458c64d34a0521adc69baf09
Eddy Ekofo IFUKI <ifuki@lapp.in2p3.fr>
2017-05-04 09:34:39 +0200

    Working on connecting to the server

--- chat_async/__init__.py
+++ chat_async/__init__.py
@@ -12,6 +12,7 @@ def main(global_config, **settings):
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('test', '/')
     config.add_renderer('.jinja2', "pyramid_jinja2.renderer_factory")
     config.scan()
 
--- chat_async/views.py
+++ chat_async/views.py
@@ -1,13 +1,26 @@
 from pyramid.view import view_config
 from chat_async.__init__ import sio
+from pyramid.request import Response
+import math
+import time
 
 
-@sio.on('ping_from_client')
 @view_config(route_name='home', renderer='templates/mytemplate.jinja2')
-def my_view(request, sid):
-    sio.emit('pong_from_server', room=sid)
-    print("The Room", sid)
+def my_view(request):
     return {'project': 'chat_async'}
+
+
+@view_config(route_name='test')
+def view(request, sid):
+    res = Response()
+    res = sio.emit("data", room=sid)
+
+    cnt = 0
+    while True:
+        cnt += 1
+        tm = time.time()
+        return sio.emit("sine", {"value": (tm * 1000,
+                                           math.sin(tm))})
 
 
 # Async
